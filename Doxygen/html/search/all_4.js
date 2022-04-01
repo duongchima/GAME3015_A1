919a1ae2f5c6fad195d0b936fd15b049af6da93e
chi <ma.duongchi@gmail.com>
2022-04-01 14:27:11 -0400

    Created new Doxygen with main page

--- Doxygen/html/search/all_4.js
+++ Doxygen/html/search/all_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['game_0',['Game',['../class_game.html',1,'']]]
+  ['game_0',['Game',['../class_game.html',1,'']]],
+  ['game3015_2da2_1',['GAME3015-A2',['../index.html',1,'']]]
 ];
---
+++ Doxygen/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['game3015_2da2_0',['GAME3015-A2',['../index.html',1,'']]]
+];
